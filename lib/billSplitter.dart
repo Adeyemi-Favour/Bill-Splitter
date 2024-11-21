b50fa543ccd5de08e3533e013c5527f1a9b2a5a9
Adeyemi-Favour <adeyemifavour844@gmail.com>
2024-11-21 13:28:25 +0100

    Label adjustment

--- lib/billSplitter.dart
+++ lib/billSplitter.dart
@@ -21,7 +21,7 @@ class _BillSplitterState extends State<BillSplitter> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Bill Spiltter',
+          'Bill Splitter',
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
         ),
       ),
